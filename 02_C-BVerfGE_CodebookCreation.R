3b04cf8c6ca162f9d5d1bb8193cd3fa5499f75cd
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-05-22 04:25:13 +0200

    replace proj shortname

--- 02_C-BVerfGE_CodebookCreation.R
+++ 02_C-BVerfGE_CodebookCreation.R
@@ -12,7 +12,7 @@
 #'    pandoc_args: --listings
 #'    includes:
 #'      in_header: tex/Preamble_DE.tex
-#'      before_body: [temp/CE-BVerfG_Definitions.tex, tex/CE-BVerfG_CodebookTitle.tex]
+#'      before_body: [temp/C-BVerfGE_Definitions.tex, tex/C-BVerfGE_CodebookTitle.tex]
 #'bibliography: temp/packages.bib
 #'nocite: '@*'
 #' ---
